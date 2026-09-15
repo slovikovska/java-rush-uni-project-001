47f972006fb20f0a1516f4bb81187739d1620597
ch_Slovikovska <36312904+slovikovska@users.noreply.github.com>
2026-09-15 17:15:11 +0300

    Update Main.java

--- src/main/java/module01/lecture11/Main.java
+++ src/main/java/module01/lecture11/Main.java
@@ -1,13 +1,11 @@
 package module01.lecture11;
 
 public class Main {
-    static void main() {
+    public static void main() {
         int a = 1;
         int b = 100;
         System.out.println(a + b);
     }
 
-    static void main() {
-        System.out.println("hello!");
-    }
+    
 }
